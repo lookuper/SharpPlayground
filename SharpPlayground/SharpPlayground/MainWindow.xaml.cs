de137e0b214ab88df862c7dc8f95837dcbcd5866
Maksym Chernenko <maksym.chernenko@gmail.com>
2015-06-17 22:34:17 +0300

    basic app flow

--- SharpPlayground/SharpPlayground/MainWindow.xaml.cs
+++ SharpPlayground/SharpPlayground/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.CodeCompletion;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,61 @@ namespace SharpPlayground
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string AppTitle = "NRefactory Code Completion";
+        private ICSharpCode.CodeCompletion.CSharpCompletion completion;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+
+            completion = new ICSharpCode.CodeCompletion.CSharpCompletion(new ScriptProvider());
+            OpenFile(@"..\SampleFiles\Sample1.cs");
+            OpenFile(@"..\SampleFiles\SampleScript1.csx");
+        }
+
+        private void OnFileOpenClick(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".cs"; // Default file extension 
+            dlg.Filter = "C# Files|*.cs?|All Files|*.*"; // Filter files by extension 
+
+            if (dlg.ShowDialog() == true)
+            {
+                OpenFile(dlg.FileName);
+            }
+        }
+
+        private void OnSaveFileClick(object sender, RoutedEventArgs e)
+        {
+            var tabItem = tabs.SelectedItem as TabItem;
+            if (tabItem == null) return;
+            var editor = tabItem.Content as CodeTextEditor;
+
+            if (editor != null && editor.SaveFile())
+            {
+                MessageBox.Show("File Saved" + Environment.NewLine + editor.FileName);
+            }
+        }
+
+        private void OpenFile(string fileName)
+        {
+            var editor = new CodeTextEditor();
+            editor.FontFamily = new FontFamily("Consolas");
+            editor.FontSize = 12;
+            editor.Completion = completion;
+            editor.OpenFile(fileName);
+            editor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinition("C#");
+
+            var tabItem = new TabItem();
+            tabItem.Content = editor;
+            tabItem.Header = System.IO.Path.GetFileName(fileName);
+            tabs.Items.Add(tabItem);
+
         }
     }
 }
---
+++ SharpPlayground/SharpPlayground/ScriptProvider.cs
@@ -0,0 +1,27 @@
+﻿using ICSharpCode.CodeCompletion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpPlayground
+{
+    class ScriptProvider : ICSharpScriptProvider
+    {
+        public string GetUsing()
+        {
+            return "" +
+                "using System; " +
+                "using System.Collections.Generic; " +
+                "using System.Linq; " +
+                "using System.Text; ";
+        }
+
+
+        public string GetVars()
+        {
+            return null;
+        }
+    }
+}
