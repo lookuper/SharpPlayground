c1dfdff65fcd6b1ad07b7c758eb874f757c55a17
Maksym Chernenko <maksym.chernenko@gmail.com>
2015-06-26 21:20:46 +0300

    add syntaxt tree diagnostic result class

---
+++ SharpPlayground/CommonTypes/SyntaxTreeDiagnosticResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTypes
+{
+    public class SyntaxTreeDiagnosticResult
+    {
+        public int LineNumber { get; set; }
+        public int LinePosition { get; set; }
+        public string Message { get; set; }
+
+        public SyntaxTreeDiagnosticResult(string diagnosticString)
+        {
+            if (String.IsNullOrEmpty(diagnosticString))
+                throw new ArgumentException(nameof(diagnosticString));
+
+            var splitted = diagnosticString.Split(':');
+
+            if (splitted.Count() < 3)
+                throw new ArgumentException("diagnostic string in unexpected format");
+
+            var numbersInParentesis = splitted[0].Split('(', ')')[1];
+
+            LineNumber = Int32.Parse(numbersInParentesis.Split(',').First());
+            LinePosition = Int32.Parse(numbersInParentesis.Split(',').Last());
+            Message = splitted.Last();
+        }
+    }
+}
--- SharpPlayground/PlaygroundCompiler/PlaygroundCompilerFacade.cs
+++ SharpPlayground/PlaygroundCompiler/PlaygroundCompilerFacade.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CodeAnalysis.CSharp;
+﻿using CommonTypes;
+using Microsoft.CodeAnalysis.CSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,12 +28,31 @@ namespace PlaygroundCompiler
         public IEnumerable<String> GetSourceCodeDiagnostics(string sourceCode)
         {
             var syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
-            var diagMessages = syntaxTree.GetDiagnostics()?
+            var dig = syntaxTree.GetDiagnostics();
+            var diagMessages = dig?
                 .Select(d => d.ToString())
                 .ToList();
 
+            var diagMessages2 = dig?
+    .Select(d => new SyntaxTreeDiagnosticResult(d.ToString()))
+    .ToList();
+            //diagMessages.First().Substring(1, diagMessages.First().IndexOf(',') - 1)
             if (diagMessages == null || diagMessages.Count == 0)
                 return new List<String>();
+
+            return diagMessages;
+        }
+
+        public IList<SyntaxTreeDiagnosticResult> GetSourceCodeDiagnostics(string sourceCode, bool t)
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
+            var dig = syntaxTree.GetDiagnostics();
+            var diagMessages = dig?
+                .Select(d => new SyntaxTreeDiagnosticResult(d.ToString()))
+                .ToList();
+            //diagMessages.First().Substring(1, diagMessages.First().IndexOf(',') - 1)
+            if (diagMessages == null || diagMessages.Count == 0)
+                return new List<SyntaxTreeDiagnosticResult>();
 
             return diagMessages;
         }
