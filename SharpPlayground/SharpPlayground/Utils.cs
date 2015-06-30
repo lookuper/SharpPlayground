a2ad4e02d5119e4e098b8842d27eabacdf54ed23
Maksym Chernenko <maksym.chernenko@gmail.com>
2015-06-30 21:00:35 +0300

    add async helper

---
+++ SharpPlayground/SharpPlayground/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SharpPlayground
+{
+    public class Utils
+    {
+        public static void Async<T>(Func<T> operation, Action<T> marshalToUI)
+        {
+            var dispatcher = Application.Current.Dispatcher;
+
+            Task.Factory.StartNew(operation)
+                .ContinueWith(result => dispatcher.BeginInvoke(marshalToUI, result.Result), TaskContinuationOptions.OnlyOnRanToCompletion);
+        }
+    }
+}
