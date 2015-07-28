6740b38068d205fcbafd305f4c451d55c8d394eb
Maksym Chernenko <maksym.chernenko@gmail.com>
2015-07-28 23:12:08 +0300

    create async playground view model

---
+++ SharpPlayground/SharpPlayground/ReactivePlaygroundViewModel.cs
@@ -0,0 +1,19 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpPlayground
+{
+    public class ReactivePlaygroundViewModel : ReactiveObject
+    {
+        private string _sourceCode;
+        public string SourceCode
+        {
+            get { return _sourceCode; }
+            set { this.RaiseAndSetIfChanged(ref _sourceCode, value); }
+        }
+    }
+}
